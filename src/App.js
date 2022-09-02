fd1caf5f5929022e9abc5d09ac0016b30f55a415
Visishta-ch <Visishtachatla@gmail.com>
2022-09-02 23:33:56 +0530

    added props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses  = [
+  {item:1, category:'stationary',amount:250, locationSpent : "Books"},
+  {item:2,category:'Petrol',amount:500, locationSpent : "Hp petrol bunk"},
+  {item:3, category:'Current Bill',amount:450, locationSpent : "Households"},
+  {item:4, category:'party',amount:1500, locationSpent : "Bday"},
+  {item:5, category:'Food',amount:250, locationSpent : "swiggy"},
+  {item:6, category:'Food',amount:250, locationSpent : "zomato"}
+
+  ]
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem>
+       {expenses.map((expense,index) =>
+        <ExpenseItem item= {expense.item}category={expense.category} amount={expense.amount} locationSpent={expense.locationSpent}>
         
-      </ExpenseItem>
+        </ExpenseItem>
+      )}
+      
     </div>
   );
 }
