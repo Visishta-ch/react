1f5ac9604271fcb7122853609659c5f664827854
Visishta-ch <Visishtachatla@gmail.com>
2022-09-02 13:14:53 +0530

    added ExpenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return (
+        <div>
+            <header> <h2>ExpenseItem</h2></header>
+            <p>Food Rs250 </p>
+            <p>Petrol Rs100 </p>
+            <p>Movie Rs200 </p>
+        </div>
+    )
+}
+
+export default ExpenseItem;
