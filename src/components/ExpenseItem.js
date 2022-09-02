3bdea946f051c32989dcd6b0c010189241ef024f
Visishta-ch <Visishtachatla@gmail.com>
2022-09-02 23:29:23 +0530

    added location using props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,21 @@
-function ExpenseItem(){
+import './ExpenseItem.css';
+function ExpenseItem(props){
+     
     return (
-        <div>
-            <header> <h2>ExpenseItem</h2></header>
-            <p>Food Rs250 </p>
-            <p>Petrol Rs100 </p>
-            <p>Movie Rs200 </p>
+        <div >
+
+            
+            <div className="expense-item"> {props.item}
+            <div className="expense-item__description">
+                <h3>{props.category}</h3>
+                <div className="expense-item__price">
+                    {props.amount}
+                </div>
+                <div className="expense-item__location">
+                <h3>{props.locationSpent}</h3>
+                </div>
+           </div>
+           </div>
         </div>
     )
 }
