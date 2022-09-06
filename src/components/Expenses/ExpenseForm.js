08f86f94c95536aa07fbb77352f693ed2dc02c64
Visishta-ch <Visishtachatla@gmail.com>
2022-09-07 00:43:51 +0530

    added form submission functionality

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -25,23 +25,32 @@ const ExpenseForm = () => {
         console.log(event.target.value)
     }
 
-    const submitDetails = () => {
-        console.log("Submitted Expense Details :")
-        console.log(document.getElementById('title').value);
-        console.log(document.getElementById('amount').value);
-         console.log(document.getElementById('date').value);
+    // const submitDetails = () => {
+    //     console.log("Submitted Expense Details :")
+    //     console.log(document.getElementById('title').value);
+    //     console.log(document.getElementById('amount').value);
+    //     console.log(document.getElementById('date').value);
         
+    // }
+    const submitDetails = (event) => {
+        event.preventDefault();
+        const dataEntered = {
+            title : titleEntered,
+            amount : amountEntered,
+            date: new Date(dateEntered)
+        }
+        console.log(dataEntered);
     }
     return (
         <div>
-        <form>
+        <form onSubmit={submitDetails}>
             <label>Expense Title</label>
             <input type = 'text' onChange={titleHandler} id ='title' placeholder = 'Expense Title' name='description' />
             <label>Amount</label>
             <input type = 'number' onChange={amountHandler} id ='amount'  placeholder = 'Expense Amount' name='amount' />
             <label>Date</label>
             <input type = 'date' onChange={dateHandler} id ='date' placeholder = 'Date' name='date' />
-            <button onClick={submitDetails}>Add Expense</button>
+            <button type = 'submit' >Add Expense</button>
           
         </form>
         </div>
